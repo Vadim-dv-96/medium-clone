36f020227b870e1a96297e04760203a3e23f3471
Vadim <matuskinvadim198@gmail.com>
2022-04-30 21:41:58 +0300

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { TopBar } from "./components/topBar";
+import { Article } from "./pages/article/article";
+import { Auth } from "./pages/auth/auth";
+import { GlobalFeed } from "./pages/globalFeed/globalFeed";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar/>
+      <Routes>
+        <Route path="/" element={<GlobalFeed />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/register" element={<Auth />} />
+        <Route path="/articles/:slug" element={<Article />} />
+      </Routes>
     </div>
   );
 }
